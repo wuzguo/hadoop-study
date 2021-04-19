4a0e4a356764b68dac8be27ad66ac48aa71968b3
wuzguo <wuzguo@gmail.com>
2021-04-19 14:51:44 +0800

    Scala

--- hadoop-study-scala/src/main/scala/com/sunvalley/study/scala/chapter20/RationalClass.scala
+++ hadoop-study-scala/src/main/scala/com/sunvalley/study/scala/chapter20/RationalClass.scala
@@ -12,7 +12,7 @@ class RationalClass(n: Int, d: Int) extends {
     val numerArg = n
     val denomArg = d
 } with RationalTrait {
-    def + (that: RationalClass) = new RationalClass(
+    def +(that: RationalClass) = new RationalClass(
         numer * that.denom + that.numer * denom,
         denom * that.denom
     )
