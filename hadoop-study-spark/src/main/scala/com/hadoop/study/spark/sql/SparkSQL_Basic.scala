e0015ddb55443a71a65188cf58bfed1c2664427a
wuzguo <wuzguo@gmail.com>
2021-05-28 14:47:08 +0800

    feat: spark sql

--- hadoop-study-spark/src/main/scala/com/hadoop/study/spark/sql/SparkSQL_Basic.scala
+++ hadoop-study-spark/src/main/scala/com/hadoop/study/spark/sql/SparkSQL_Basic.scala
@@ -1,7 +1,8 @@
 package com.hadoop.study.spark.sql
 
 import org.apache.spark.SparkConf
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 
 /**
  * <B>说明：描述</B>
@@ -16,8 +17,55 @@ object SparkSQL_Basic {
     def main(args: Array[String]): Unit = {
         val conf = new SparkConf().setAppName("sparkSQL").setMaster("local[*]")
         val spark = SparkSession.builder().config(conf).getOrCreate()
-        val frame = spark.read.json("./hadoop-study-datas/spark/sql/user.json")
-        frame.show()
+        // 读取文件
+        val df = spark.read.json("./hadoop-study-datas/spark/sql/user.json")
+
+        // 1. 创建全局视图
+        df.createGlobalTempView("user")
+        spark.sql("select * from global_temp.user").show()
+
+        // 2. 创建Session的视图
+        df.createOrReplaceTempView("user")
+        spark.sql("select * from user").show()
+
+        // 3. 添加函數
+        spark.sql("select avg(age) from user").show()
+
+        // 4. DSL指定列
+        df.select("name", "age").show()
+
+        // 5. 結果运算
+        import spark.implicits._
+        df.select('age + 1).show()
+        df.select($"age" + 1).show()
+
+        // 6. DataSet
+        val seq = Seq(1, 2, 3, 4)
+        val ds: Dataset[Int] = seq.toDS()
+        ds.show()
+
+        // 7. RDD <=> DataFrame
+        val rdd = spark.sparkContext.makeRDD(List((1, "zhangsan", 30), (2, "lisi", 40)))
+        val df2 = rdd.toDF("id", "name", "age")
+        val rowRDD: RDD[Row] = df2.rdd
+        // [1,zhangsan,30]
+        // [2,lisi,40]
+        rowRDD.collect().foreach(println)
+
+
+        // 8. RDD <=> DataSet
+        val ds1: Dataset[User] = rdd.map {
+            case (id, name, age) =>
+                User(id, name, age)
+        }.toDS()
+        val userRDD: RDD[User] = ds1.rdd
+        // User(1,zhangsan,30)
+        // User(2,lisi,40)
+        userRDD.collect().foreach(println)
+
         spark.stop()
     }
+
+
+    case class User(id: Int, name: String, age: Int)
 }
