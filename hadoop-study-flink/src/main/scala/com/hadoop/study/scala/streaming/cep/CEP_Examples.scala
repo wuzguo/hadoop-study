6cae217b9a5683265491e31746a1d615a3cd53a9
wuzguo <wuzguo@gmail.com>
2021-06-24 14:44:26 +0800

    cep

---
+++ hadoop-study-flink/src/main/scala/com/hadoop/study/scala/streaming/cep/CEP_Examples.scala
@@ -0,0 +1,16 @@
+package com.hadoop.study.scala.streaming.cep
+
+/**
+ * <B>说明：描述</B>
+ *
+ * @author zak.wu
+ * @version 1.0.0
+ * @date 2021/6/24 14:43
+ */
+
+object CEP_Examples {
+
+    def main(args: Array[String]): Unit = {
+        
+    }
+}
