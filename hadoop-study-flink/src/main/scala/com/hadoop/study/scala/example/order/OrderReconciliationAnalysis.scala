f03ddc2cdf4c9d241c490d245006686e37f5a8fd
wuzguo <wuzguo@gmail.com>
2021-06-25 08:57:33 +0800

    order 对账

---
+++ hadoop-study-flink/src/main/scala/com/hadoop/study/scala/example/order/OrderReconciliationAnalysis.scala
@@ -0,0 +1,17 @@
+package com.hadoop.study.scala.example.order
+
+/**
+ * <B>说明：描述</B>
+ *
+ * @author zak.wu
+ * @version 1.0.0
+ * @date 2021/6/25 8:56
+ */
+
+object OrderReconciliationAnalysis {
+
+    def main(args: Array[String]): Unit = {
+        
+    }
+
+}
