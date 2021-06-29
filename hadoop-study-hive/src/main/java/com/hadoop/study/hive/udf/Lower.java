61bc81fe2b51e20299f41fe9df1555baedde3a9a
wuzguo <wuzguo@gmail.com>
2021-06-29 16:56:33 +0800

    feat: UDF

---
+++ hadoop-study-hive/src/main/java/com/hadoop/study/hive/udf/Lower.java
@@ -0,0 +1,18 @@
+package com.hadoop.study.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * <B>说明：描述</B>
+ *
+ * @author zak.wu
+ * @version 1.0.0
+ * @date 2021/6/29 15:49
+ */
+
+public class Lower extends UDF {
+
+    public String evaluate(final String str) {
+        return (str == null) ? null : str.toLowerCase();
+    }
+}
