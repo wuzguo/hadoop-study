a3a3bbeee7c871628a878c8f5464fa283724dada
wuzguo <wuzguo@gmail.com>
2021-07-08 10:01:46 +0800

    交易欺诈项目

---
+++ hadoop-study-flink-project-fraud-detecting/fraud-detecting-job/src/main/scala/com/hadoop/study/fraud/detect/config/Parameters.scala
@@ -0,0 +1,11 @@
+package com.hadoop.study.fraud.detect.config
+
+/**
+ * <B>说明：描述</B>
+ *
+ * @author zak.wu
+ * @version 1.0.0
+ * @date 2021/7/8 9:38
+ */
+
+case class Parameters(tool: ParameterTool)
