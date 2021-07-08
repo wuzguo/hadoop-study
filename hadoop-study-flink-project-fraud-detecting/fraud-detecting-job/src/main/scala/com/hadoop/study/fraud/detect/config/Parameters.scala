bcd865fab8abad4e978afb7864d400e4f7eba8cd
wuzguo <wuzguo@gmail.com>
2021-07-08 10:35:02 +0800

    parameters

--- hadoop-study-flink-project-fraud-detecting/fraud-detecting-job/src/main/scala/com/hadoop/study/fraud/detect/config/Parameters.scala
+++ hadoop-study-flink-project-fraud-detecting/fraud-detecting-job/src/main/scala/com/hadoop/study/fraud/detect/config/Parameters.scala
@@ -1,5 +1,7 @@
 package com.hadoop.study.fraud.detect.config
 
+import org.apache.flink.api.java.utils.ParameterTool
+
 /**
  * <B>说明：描述</B>
  *
@@ -8,4 +10,76 @@ package com.hadoop.study.fraud.detect.config
  * @date 2021/7/8 9:38
  */
 
-case class Parameters(tool: ParameterTool)
+case class Parameters(tool: ParameterTool) {
+
+    def getOrDefault[T](param: Param[T]): T = {
+        // 如果没有值直接返回
+        if (!tool.has(param.name)) return param.default
+
+        val value =
+            if (param.cls eq classOf[Integer]) tool.getInt(param.name)
+            else if (param.cls eq classOf[Long]) tool.getLong(param.name)
+            else if (param.cls eq classOf[Double]) tool.getDouble(param.name)
+            else if (param.cls eq classOf[Boolean]) tool.getBoolean(param.name)
+            else tool.get(param.name)
+
+        param.cls.cast(value)
+    }
+}
+
+object Parameters {
+
+    def fromArgs(args: Array[String]): Parameters = {
+        val tool = ParameterTool.fromArgs(args)
+        new Parameters(tool)
+    }
+
+    // Kafka:
+    val KAFKA_HOST: Param[String] = Param.string("kafka-host", "localhost")
+    val KAFKA_PORT: Param[Integer] = Param.integer("kafka-port", 9092)
+
+    val DATA_TOPIC: Param[String] = Param.string("data-topic", "live-transactions")
+    val ALERTS_TOPIC: Param[String] = Param.string("alerts-topic", "alerts")
+    val RULES_TOPIC: Param[String] = Param.string("rules-topic", "rules")
+    val LATENCY_TOPIC: Param[String] = Param.string("latency-topic", "latency")
+    val RULES_EXPORT_TOPIC: Param[String] = Param.string("current-rules-topic", "current-rules")
+
+    val OFFSET: Param[String] = Param.string("offset", "latest")
+
+    // GCP PubSub:
+    val GCP_PROJECT_NAME: Param[String] = Param.string("gcp-project", "da-fe-212612")
+    val GCP_PUBSUB_RULES_SUBSCRIPTION: Param[String] = Param.string("pubsub-rules", "rules-demo")
+    val GCP_PUBSUB_ALERTS_SUBSCRIPTION: Param[String] = Param.string("pubsub-alerts", "alerts-demo")
+    val GCP_PUBSUB_LATENCY_SUBSCRIPTION: Param[String] = Param.string("pubsub-latency", "latency-demo")
+    val GCP_PUBSUB_RULES_EXPORT_SUBSCRIPTION: Param[String] = Param.string("pubsub-rules-export", "current-rules-demo")
+
+    // Socket
+    val SOCKET_PORT: Param[Integer] = Param.integer("pubsub-rules-export", 9999)
+
+    // General:
+    // source/sink types: kafka / pubsub / socket
+    val RULES_SOURCE: Param[String] = Param.string("rules-source", "SOCKET")
+    val TRANSACTIONS_SOURCE: Param[String] = Param.string("data-source", "GENERATOR")
+    val ALERTS_SINK: Param[String] = Param.string("alerts-sink", "STDOUT")
+    val LATENCY_SINK: Param[String] = Param.string("latency-sink", "STDOUT")
+    val RULES_EXPORT_SINK: Param[String] = Param.string("rules-export-sink", "STDOUT")
+
+    val RECORDS_PER_SECOND: Param[Integer] = Param.integer("records-per-second", 2)
+
+    val LOCAL_MODE_DISABLE_WEB_UI = "-1"
+
+    val LOCAL_EXECUTION: Param[String] = Param.string("local", LOCAL_MODE_DISABLE_WEB_UI)
+
+    val SOURCE_PARALLELISM: Param[Integer] = Param.integer("source-parallelism", 2)
+    val SINK_PARALLELISM: Param[Integer] = Param.integer("sink-parallelism", 1)
+    val CHECKPOINT_INTERVAL: Param[Integer] = Param.integer("checkpoint-interval", 600000)
+    val MIN_PAUSE_BETWEEN_CHECKPOINTS: Param[Integer] = Param.integer("min-pause-btwn-checkpoints", 600000)
+    val OUT_OF_ORDERLESS: Param[Integer] = Param.integer("out-of-orderliness", 500)
+
+
+    val STRING_PARAMS = List(LOCAL_EXECUTION, KAFKA_HOST, DATA_TOPIC, ALERTS_TOPIC, RULES_TOPIC, LATENCY_TOPIC, RULES_EXPORT_TOPIC, OFFSET, GCP_PROJECT_NAME, GCP_PUBSUB_RULES_SUBSCRIPTION, GCP_PUBSUB_ALERTS_SUBSCRIPTION, GCP_PUBSUB_LATENCY_SUBSCRIPTION, GCP_PUBSUB_RULES_EXPORT_SUBSCRIPTION, RULES_SOURCE, TRANSACTIONS_SOURCE, ALERTS_SINK, LATENCY_SINK, RULES_EXPORT_SINK)
+
+    val INT_PARAMS = List(KAFKA_PORT, SOCKET_PORT, RECORDS_PER_SECOND, SOURCE_PARALLELISM, SINK_PARALLELISM, CHECKPOINT_INTERVAL, MIN_PAUSE_BETWEEN_CHECKPOINTS, OUT_OF_ORDERLESS)
+
+    val BOOL_PARAMS: List[Param[Boolean]] = List[Param[Boolean]]()
+}
