e86bcd75c407b53ae3fef4e5183616e2506b0eff
wuzguo <wuzguo@gmail.com>
2021-07-12 17:08:33 +0800

    feat: 前端页面

--- hadoop-study-flink-project-fraud-detecting/fraud-detecting-web/src/main/java/com/hadoop/study/fraud/detect/DetectApplication.java
+++ hadoop-study-flink-project-fraud-detecting/fraud-detecting-web/src/main/java/com/hadoop/study/fraud/detect/DetectApplication.java
@@ -1,7 +1,7 @@
 package com.hadoop.study.fraud.detect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -13,7 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@Slf4j
 public class DetectApplication {
 
     public static void main(String[] args) {
