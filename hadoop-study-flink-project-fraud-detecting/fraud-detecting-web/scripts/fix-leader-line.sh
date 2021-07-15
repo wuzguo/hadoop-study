5afe378b236e7d42ba70241ad902570f280d9314
wuzguo <wuzguo@gmail.com>
2021-07-15 13:38:17 +0800

    功能调试

--- hadoop-study-flink-project-fraud-detecting/fraud-detecting-web/scripts/fix-leader-line.sh
+++ hadoop-study-flink-project-fraud-detecting/fraud-detecting-web/scripts/fix-leader-line.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if grep -q 'module.exports = LeaderLine' ./node_modules/leader-line/leader-line.min.js; then
+if grep -q 'module.exports = LeaderLine' ../node_modules/leader-line/leader-line.min.js; then
   echo 'Leader Line already patched'
 else
   echo "\
     if (module && module.exports) { module.exports = LeaderLine }\
-  " >> ./node_modules/leader-line/leader-line.min.js
+  " >> ../node_modules/leader-line/leader-line.min.js
   echo "Fixed LeaderLine!"
 fi;
 
