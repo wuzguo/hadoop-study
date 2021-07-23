369261093d2482cbdc8610670e4e038c225fa2d6
wuzguo <wuzguo@gmail.com>
2021-07-23 14:50:00 +0800

    recommend

---
+++ hadoop-study-flink-project-recommend/recommend-flink-job/src/main/scala/com/hadoop/study/flink/recommend/OfflineRecommender.scala
@@ -0,0 +1,16 @@
+package com.hadoop.study.flink.recommend
+
+/**
+ * <B>说明：描述</B>
+ *
+ * @author zak.wu
+ * @version 1.0.0
+ * @date 2021/7/23 14:49
+ */
+
+object OfflineRecommender {
+
+    def main(args: Array[String]): Unit = {
+
+    }
+}
