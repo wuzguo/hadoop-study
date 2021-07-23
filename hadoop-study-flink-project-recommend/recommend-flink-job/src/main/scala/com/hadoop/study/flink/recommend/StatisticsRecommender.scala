3374fd379e25a370a3738032e877984918660110
wuzguo <wuzguo@gmail.com>
2021-07-23 14:03:57 +0800

    recommend

--- hadoop-study-flink-project-recommend/recommend-flink-job/src/main/scala/com/hadoop/study/flink/recommend/StatisticsRecommender.scala
+++ hadoop-study-flink-project-recommend/recommend-flink-job/src/main/scala/com/hadoop/study/flink/recommend/StatisticsRecommender.scala
@@ -1,11 +1,15 @@
 package com.hadoop.study.flink.recommend
 
-import com.hadoop.study.flink.recommend.sinks.RateProductMongoSink
+import com.hadoop.study.flink.recommend.beans.Rating
+import com.hadoop.study.flink.recommend.sinks.{RateProductMongoSink, RateRecentlyProductMongoSink}
 import com.hadoop.study.flink.recommend.sources.RatingMongoSource
-import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, createTypeInformation}
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment, createTypeInformation}
 import org.apache.flink.table.api.FieldExpression
 import org.apache.flink.table.api.bridge.scala.{StreamTableEnvironment, tableConversions}
 import org.apache.flink.types.Row
+
+import java.text.SimpleDateFormat
+import java.util.Date
 
 /**
  * <B>说明：描述</B>
@@ -22,30 +26,46 @@ object StatisticsRecommender {
         val env = StreamExecutionEnvironment.getExecutionEnvironment
         // 2、添加自定义source  连接mongodb
         val dataStream = env.addSource(RatingMongoSource("recommender", "ratings"))
-        // 3、打印数据
-        // dataStream.print()
 
         // 4. 创建表环境
         val tableEnv = StreamTableEnvironment.create(env)
-        val tableRating = tableEnv.fromDataStream(dataStream, $"userId", $"productId")
-        tableRating.printSchema()
-        // 5. 创建视图，执行SQL
-        tableEnv.createTemporaryView("ratings", tableRating)
-
         // 6.1 历史热门
-        historyHotProducts(tableEnv)
+        historyHotProducts(tableEnv, dataStream)
+        // 6.2 近期热门
+        recentHotProducts(tableEnv, dataStream)
 
         // 7、执行任务
         env.execute("statistics recommender")
     }
 
     // 历史热门
-    def historyHotProducts(tableEnv: StreamTableEnvironment): Unit = {
+    def historyHotProducts(tableEnv: StreamTableEnvironment, ratingStream: DataStream[Rating]): Unit = {
+        val tableRating = tableEnv.fromDataStream(ratingStream, $"userId", $"productId")
+        // 5. 创建视图，执行SQL
+        tableEnv.createTemporaryView("ratings", tableRating)
         // 只保存前 100 热门数据
         val sql = "select productId, count(productId) as counts from ratings group by productId order by counts desc limit 100"
         // 执行
         val table = tableEnv.sqlQuery(sql)
         // 写表
-        table.toRetractStream[Row].addSink(RateProductMongoSink("recommender", "rate_products"))
+        table.toRetractStream[Row].addSink(RateProductMongoSink("recommender", "rate_products")).setParallelism(1)
+    }
+
+    // 近期热门
+    def recentHotProducts(tableEnv: StreamTableEnvironment, ratingStream: DataStream[Rating]): Unit = {
+        val sourceStream = ratingStream.map(rating => {
+            val format = new SimpleDateFormat("yyyyMM")
+            val time = format.format(new Date(rating.timestamp * 1000L))
+            (rating.userId, rating.productId, time.toInt)
+        })
+
+        val tableRating = tableEnv.fromDataStream(sourceStream, $"userId", $"productId", $"yearMonth")
+        // 5. 创建视图，执行SQL
+        tableEnv.createTemporaryView("recentRatings", tableRating)
+        val sql = "select productId, count(productId) as counts, yearMonth from recentRatings group by yearMonth, productId order by yearMonth desc, counts desc"
+        // 执行
+        val table = tableEnv.sqlQuery(sql)
+        // 写表
+        table.toRetractStream[Row].addSink(RateRecentlyProductMongoSink("recommender", "rate_recently_products")).setParallelism(1)
     }
 }
---
+++ hadoop-study-flink-project-recommend/recommend-flink-job/src/main/scala/com/hadoop/study/flink/recommend/sinks/RateRecentlyProductMongoSink.scala
@@ -0,0 +1,34 @@
+package com.hadoop.study.flink.recommend.sinks
+
+import com.hadoop.study.flink.recommend.utils.ConnHelper
+import com.mongodb.casbah.MongoCollection
+import com.mongodb.casbah.commons.MongoDBObject
+import org.apache.flink.configuration.Configuration
+import org.apache.flink.streaming.api.functions.sink.{RichSinkFunction, SinkFunction}
+import org.apache.flink.types.Row
+
+/**
+ * <B>说明：描述</B>
+ *
+ * @author zak.wu
+ * @version 1.0.0
+ * @date 2021/7/22 15:07
+ */
+
+case class RateRecentlyProductMongoSink(db: String, collection: String) extends RichSinkFunction[(Boolean, Row)] {
+
+    private var mongoCollection: MongoCollection = _
+
+    override def open(parameters: Configuration): Unit = mongoCollection = ConnHelper.mongoClient(db)(collection)
+
+    override def close(): Unit = mongoCollection = null
+
+    override def invoke(value: (Boolean, Row), context: SinkFunction.Context): Unit = {
+        // 删除数据
+        mongoCollection.remove(MongoDBObject("productId" -> value._2.getField(0)))
+        // 保存数据
+        mongoCollection.insert(MongoDBObject("productId" -> value._2.getField(0),
+            "count" -> value._2.getField(1),
+            "yearMonth" -> value._2.getField(2)))
+    }
+}
